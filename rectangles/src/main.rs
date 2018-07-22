e6890922710e59fb1b9e7eab5a31c85d5cec469b
Michael Brett <michael.brett.webdev@gmail.com>
2018-07-22 11:11:16 +0100

    Add debug trait

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,3 +1,5 @@
+#[derive(Debug)]
+
 struct Rectangle {
     width: u32,
     height: u32,
@@ -8,6 +10,8 @@ fn main() {
         width: 30,
         height: 50,
     };
+
+    println!("rect is {:#?}", rect1);
 
     println!(
         "The area of the rectangle is {} square pixels,",
